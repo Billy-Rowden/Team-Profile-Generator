4567b72daa17023701220969e4b80444dd72f45d
Billy Rowden <williampsr.rowden@gmail.com>
2024-02-13 22:21:53 +0000

    added an empty array for teamMembers and an array of objects for questions for the manager team member

--- index.js
+++ index.js
@@ -10,3 +10,28 @@ const Intern = require("./lib/Intern");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const teamMembers = [];
+
+const managerQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "Enter team manager's name:",
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "Enter team manager's ID:",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Enter team manager's email:",
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: "Enter team manager's office number:",
+  },
+];
